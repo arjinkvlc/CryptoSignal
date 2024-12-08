ab5d946e54ed9996c30319259428ff63edf6d7cd
Arjin Kavalcı <arjin.33@outlook.com>
2024-12-08 18:47:35 +0300

    initial submit

---
+++ CryptoSignal-backend/config.py
@@ -0,0 +1,4 @@
+BINANCE_API_KEY = ""
+BINANCE_SECRET_KEY = ""
+BASE_URL = "https://api.binance.com/api"  # Spot Mainnet
+COINS = ["ETHUSDT", "BTCUSDT", "AVAXUSDT", "SOLUSDT", "RENDERUSDT", "FETUSDT"]
